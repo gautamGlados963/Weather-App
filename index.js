2ebbde6ceff87d9e1dbe0c54b7786a1ccef1eef0
gautamGlados963 <gautamgp96300@outlook.com>
2024-06-15 22:12:47 +0530

    Added functionality with api connection to get weather data.
    Added search button functionality on clicking to show the weather data.

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import { getWeather } from "./module.js";
 
-// const city = document.querySelector(".search input");
-const searchBtn = document.querySelector(".search button");
+document.addEventListener('DOMContentLoaded', function () {
 
-searchBtn.addEventListener("click", ()=> {
-    const city = document.querySelector('.search input').value;
-    city.trim();
-    const data = getWeather(city)
-    .then(data => console.log(data))
-    .catch(error => console.error("Error:", error));
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
+    const searchBtn = document.querySelector(".search button");
+
+    searchBtn.addEventListener("click", () => {
+        const city = document.querySelector('.search input').value.trim();
+        getWeather(city)
+            .then(data => {
+                if (data && data.name && data.main && data.main.temp && data.wind.speed && data.main.humidity !== null) {
+
+                    document.querySelector(".city").innerHTML = String(data.name);
+                    document.querySelector(".condition").innerHTML = String(data.weather[0].main);
+                    document.querySelector(".temp").innerHTML = String(data.main.temp) + "°C";
+                    document.querySelector(".wind").innerHTML = String(data.wind.speed) + "km/h";
+                    document.querySelector(".humidity").innerHTML = String(data.main.humidity) + "%";
+
+                    let weatherCondition = data.weather[0].main.toLowerCase();
+                    document.querySelector(".weather-icon").src = `./assest/${weatherCondition}.png`;
+ 
+                } else {
+
+                    console.error("Unexpected data format:", data);
+
+                }
+                console.log(data);
+                document.querySelector(".weather").style.display = "flex";
+                document.querySelector(".info").style.display = "flex";
+            })          
+        .catch (error => console.error("Error:", error));
+
+    });
+
 })
-
