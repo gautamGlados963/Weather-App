e03651fe4d5671ab2297f6d70be96841cb1ede04
gautamGlados963 <gautamgp96300@outlook.com>
2024-06-17 18:57:21 +0530

    Added feature to detect when wrong city is entered. Fixed styles.css file.

--- index.js
+++ index.js
@@ -6,31 +6,39 @@ document.addEventListener('DOMContentLoaded', function () {
     const searchBtn = document.querySelector(".search button");
 
     searchBtn.addEventListener("click", () => {
+
         const city = document.querySelector('.search input').value.trim();
         getWeather(city)
             .then(data => {
-                if (data && data.name && data.main && data.main.temp && data.wind.speed && data.main.humidity !== null) {
+                if (data.cod == 404 || data.message == "Nothing to geocode") {
+                    document.querySelector(".error").style.display = "block";
+                    document.querySelector(".weather").style.display = "none";
+                    document.querySelector(".info").style.display = "none";
 
-                    document.querySelector(".city").innerHTML = String(data.name);
-                    document.querySelector(".condition").innerHTML = String(data.weather[0].main);
-                    document.querySelector(".temp").innerHTML = String(data.main.temp) + "°C";
-                    document.querySelector(".wind").innerHTML = String(data.wind.speed) + "km/h";
-                    document.querySelector(".humidity").innerHTML = String(data.main.humidity) + "%";
-
-                    let weatherCondition = data.weather[0].main.toLowerCase();
-                    document.querySelector(".weather-icon").src = `./assest/${weatherCondition}.png`;
- 
                 } else {
+                    if (data && data.name && data.main && data.main.temp && data.wind.speed && data.main.humidity !== null) {
 
-                    console.error("Unexpected data format:", data);
+                        document.querySelector(".city").innerHTML = String(data.name);
+                        document.querySelector(".condition").innerHTML = String(data.weather[0].main);
+                        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+                        document.querySelector(".wind").innerHTML = String(data.wind.speed) + "km/h";
+                        document.querySelector(".humidity").innerHTML = String(data.main.humidity) + "%";
 
+                        let weatherCondition = data.weather[0].main.toLowerCase();
+                        document.querySelector(".weather-icon").src = `./assest/${weatherCondition}.png`;
+
+                    } else {
+
+                        console.error("Unexpected data format:", data);
+
+                    }
+
+                    console.log(data);
+                    document.querySelector(".weather").style.display = "flex";
+                    document.querySelector(".info").style.display = "flex";
+                    document.querySelector(".error").style.display = "none";
                 }
-                console.log(data);
-                document.querySelector(".weather").style.display = "flex";
-                document.querySelector(".info").style.display = "flex";
-            })          
-        .catch (error => console.error("Error:", error));
-
+            })
+            .catch(error => console.error("Error:", error));
     });
-
 })
